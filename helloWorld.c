b0f8cf61a11a601548cd6855bbc7df2967c5b581
Ben Sleeper <sleeperb.cs@gmail.com>
2024-09-01 15:12:21 -0400

    initial commit

---
+++ helloWorld.c
@@ -0,0 +1,25 @@
+/* Hello world program */
+
+#include <stdio.h>
+
+int main(void){
+    printf("Hello, World!\n"); // Beej says here, "Actually do the work here"
+
+    //I guess that means actually writing the program?
+}
+
+/*notes
+
+
+
+
+*/
+
+/*vocabulary
+digraphs - /*
+C Preprocessor Directives - #include #define
+Octothorpe - #
+compiler - after preprocessing, compiler produces/translates C into assembly/machine code
+header file - .h: 
+s
+*/
